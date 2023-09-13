b85878dca84c23a7f817aebfdfec92c9465bf2df
Minu <122645470+Minusuke@users.noreply.github.com>
2023-09-13 22:46:52 +0000

    revisado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+document.getElementById('myForm').addEventListener('submit', function(event) {
+  event.preventDefault(); // Detener el envío predeterminado del formulario
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  const nombre = document.getElementById('nombre').value;
+  const email = document.getElementById('email').value;
+  const errorMsg = document.getElementById('error-msg');
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  // Validar si los campos están vacíos
+  if (nombre === '' || email === '') {
+    errorMsg.innerText = 'Por favor, complete todos los campos';
+    return;
+  }
+
+  // Validar si el formato del email es correcto
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    errorMsg.innerText = 'Por favor, ingrese un email válido';
+    return;
+  }
+
+  // Si todo es válido, limpiar el mensaje de error y enviar el formulario
+  errorMsg.innerText = '';
+  document.getElementById('myForm').submit();
+});
